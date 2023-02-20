65153135c9d5a46978e23991d00a047949074620
Shr4528 <Shr4528@gmail.com>
2023-02-20 12:31:33 +0200

    commit

--- controllers/property.controller.js
+++ controllers/property.controller.js
@@ -48,7 +48,16 @@ const getAllProperties = async (req, res) => {
 
 
 };
-const getPropertyDetail = async (req, res) => { };
+const getPropertyDetail = async (req, res) => {
+    const { id } = res.params;
+    const propertyExists = await Property.findOne({ _id: id }).populate('creator');
+
+    if (propertyExists) {
+        res.status(200).json(propertyExists);
+    } else {
+        res.status(404).json({ message: 'Property not found' });
+    }
+};
 
 const createProperty = async (req, res) => {
     try {
