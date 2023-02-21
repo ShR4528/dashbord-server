77ece8dcfef0eb6d8aa444674a0a1df4464869f9
Shr4528 <Shr4528@gmail.com>
2023-02-21 19:10:08 +0200

    end

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,12 +1,12 @@
 import express from 'express';
 
 // import all controllers
-import { getAllUsers, createUsers, getUserInfoById } from '../controllers/user.controller.js';
+import { getAllUsers, createUsers, getUserInfoByID } from '../controllers/user.controller.js';
 
 const router = express.Router();
 
 router.route('/').get(getAllUsers);
 router.route('/').post(createUsers);
-router.route('/:id').get(getUserInfoById);
+router.route('/:id').get(getUserInfoByID);
 
 export default router;
